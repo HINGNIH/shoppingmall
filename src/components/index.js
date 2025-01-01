1211c9f6569fdbf15b05da74faf56f792513e0b2
HINGNIH <dkstks1409@gmail.com>
2025-01-02 02:31:58 +0900

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as NoticeDetail} from './notice/NoticeDetail';
-export {default as CustomerDetail} from './customer/CustomerDetail';
-export {default as CustomerAdd} from './customer/CustomerAdd';
-export {default as CustomerEdit} from './customer/CustomerEdit';
